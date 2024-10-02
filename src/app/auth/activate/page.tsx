0317f61b4ace6be65ad57c13286fd085cc54a3bc
Jakub <pecet3107@gmail.com>
2024-10-02 14:55:15 +0200

    ok

--- src/app/auth/activate/page.tsx
+++ src/app/auth/activate/page.tsx
@@ -1,33 +1,39 @@
-"use client"
+"use client";
 
-import { useEffect } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { Suspense, useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+function ActivatePage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const activateAccount = async () => {
+      const id = searchParams.get("code");
+      if (id) {
+        const response = await fetch(`/api/v1/auth/activate/${id}`, {
+          method: "GET",
+          credentials: "include",
+        });
+        if (response.ok) {
+          setTimeout(() => router.push("/auth/login"), 500);
+        }
+      }
+    };
+    activateAccount();
+  }, [searchParams, router]);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <p>Activating an account...</p>
+    </div>
+  );
+}
 
 export default function Page() {
-    const searchParams = useSearchParams();
-    const router = useRouter();
-
-    useEffect(() => {
-        const activateAccount = async () => {
-            const id = searchParams.get('code');
-            if (id) {
-                const response = await fetch(`/api/v1/auth/activate/${id}`, {
-                    method: 'GET',
-                    credentials: 'include',
-                });
-                if (response.ok) {
-                    setTimeout(() => (
-                        router.push('/auth/login')
-                    ), 500)
-                }
-            }
-        };
-        activateAccount();
-    }, []);
-
-    return (
-        <div className="flex justify-center items-center min-h-screen">
-            <p>Activating an account...</p>
-        </div>
-    );
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ActivatePage />
+    </Suspense>
+  );
 }
